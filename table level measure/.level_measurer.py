a5dbc9c14a0ff70a3966b84817ca4f8dec7276fe
Willy Yeremi <willyyeremi@gmail.com>
2024-01-09 15:02:46 +0700

    build: This commit refactors the script to generate a list of shell scripts for initial data loading, updates the database connection
    - Refactors existing script for generating shell scripts for initial data loading
    - Removes unnecessary libraries
    - Improves file path handling
    - Adds new database connection details for source and target databases in configuration file
    - Removes old connection details
    - Refactors database query code
    - Improves readability and performance
    - Optimizes queries
    - Removes redundant code
    - Organizes code into logical sections

--- table level measure/.level_measurer.py
+++ table level measure/.level_measurer.py
@@ -16,19 +16,20 @@ import access
 
 
 # credential
-product = 'postgres'
+product = 'mysql'
+envi = access.trg_envi
 user = access.trg_user
 pwd = access.trg_pwd
 host = access.trg_host
 port = access.trg_port
 db = access.trg_db
-schema = 'bss_billengine'
+schema = 'intranet.balitower.co.id'
 
 # current timestamp
 current_timestamp = datetime.datetime.strftime(datetime.datetime.now(), '%Y%m%d_%H%M%S')
 
 class table_and_relation:
-    def __init__(self,product,host,port,user,pwd,db,schema,envi=''):
+    def __init__(self,product,host,port,user,pwd,db,schema,envi):
         self.product = product
         self.envi = envi
         self.host = host
@@ -52,31 +53,31 @@ class table_and_relation:
         engine = sqlalchemy.create_engine(url)
         conn = engine.connect()
         relation = pandas.DataFrame(conn.execute(sqlalchemy.sql.text("""
-        SELECT
-            a.table_name AS "table_name"
-            ,c.table_name AS "references"
-        FROM
-            all_cons_columns a
-            LEFT JOIN
-            all_constraints b
-            ON a.owner = b.owner AND a.constraint_name = b.constraint_name
-            LEFT JOIN
-            all_constraints c 
-            ON b.r_owner = c.owner AND b.r_constraint_name = c.constraint_name
-        WHERE
-            b.constraint_type = 'R'
-        AND
-            a.owner = {schema}""".format(schema=self.schema))))
+            SELECT
+                a.table_name AS "table_name"
+                ,c.table_name AS "references"
+            FROM
+                all_cons_columns a
+                LEFT JOIN
+                all_constraints b
+                ON a.owner = b.owner AND a.constraint_name = b.constraint_name
+                LEFT JOIN
+                all_constraints c 
+                ON b.r_owner = c.owner AND b.r_constraint_name = c.constraint_name
+            WHERE
+                b.constraint_type = 'R'
+            AND
+            a.owner = '{schema}'""".format(schema=self.schema))))
         relation['key']=relation['table_name']+relation['references']
         relation = relation.drop_duplicates(subset=['key'])
         relation = relation.drop('key', axis=1)
         all_table = pandas.DataFrame(conn.execute(sqlalchemy.sql.text("""
-        SELECT 
-            table_name
-        FROM 
-            all_tables
-        WHERE 
-            owner = {schema}""".format(schema=self.schema))))
+            SELECT 
+                table_name
+            FROM 
+                all_tables
+            WHERE 
+                owner = '{schema}'""".format(schema=self.schema))))
         return (relation,all_table)
 
     def postgres(self):
@@ -84,53 +85,90 @@ class table_and_relation:
         engine = sqlalchemy.create_engine(url)
         conn = engine.connect()
         relation =  pandas.DataFrame(conn.execute(sqlalchemy.sql.text("""
-        select
-            c.relname as table_name
-            ,d.relname  as references
-        from
-            pg_catalog.pg_constraint as a
-            left join
-            pg_catalog.pg_namespace as b
-            on a.connamespace = b."oid" 
-            left join 
-            pg_catalog.pg_class as c
-            on a.conrelid = c."oid" 
-            left join 
-            pg_catalog.pg_class as d
-            on a.confrelid = d."oid" 
-        where 
-            a.contype = 'f'
-            and b.nspname = '{schema}'
-        order by 
-            c.relname 
-            ,d.relname
-        """.format(schema=self.schema))))
+            select
+                c.relname as table_name
+                ,d.relname  as references
+            from
+                pg_catalog.pg_constraint as a
+                left join
+                pg_catalog.pg_namespace as b
+                on a.connamespace = b."oid" 
+                left join 
+                pg_catalog.pg_class as c
+                on a.conrelid = c."oid" 
+                left join 
+                pg_catalog.pg_class as d
+                on a.confrelid = d."oid" 
+            where 
+                a.contype = 'f'
+                and b.nspname = '{schema}'
+            order by 
+                c.relname 
+                ,d.relname
+            """.format(schema=self.schema))))
         relation['key']=relation['table_name']+relation['references']
         relation = relation.drop_duplicates(subset=['key'])
         relation = relation.drop('key', axis=1)
         all_table = pandas.DataFrame(conn.execute(sqlalchemy.sql.text("""
-        SELECT 
-            table_name
-        from 
-            information_schema.tables
-        WHERE 
-            table_schema = '{schema}'
-            and table_type = 'BASE TABLE'
-        order by
-            table_name
-        """.format(schema=self.schema))))
+            SELECT 
+                table_name
+            from 
+                information_schema.tables
+            WHERE 
+                table_schema = '{schema}'
+                and table_type = 'BASE TABLE'
+            order by
+                table_name
+            """.format(schema=self.schema))))
+        return (relation,all_table)
+    
+    def mysql(self):
+        url = f"mysql+pymysql://{self.user}:{urllib.parse.quote_plus(self.pwd)}@{self.host}/{self.db}?charset=utf8mb4"
+        engine = sqlalchemy.create_engine(url)
+        conn = engine.connect()
+        relation =  pandas.DataFrame(conn.execute(sqlalchemy.sql.text("""
+            SELECT
+                TABLE_NAME as table_name
+                ,REFERENCED_TABLE_NAME as "references"
+            FROM
+                INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+            WHERE
+                TABLE_SCHEMA = '{schema}'
+                AND
+                REFERENCED_TABLE_NAME IS NOT NULL;
+            """.format(schema=self.schema))))
+        relation['key']=relation['table_name']+relation['references']
+        relation = relation.drop_duplicates(subset=['key'])
+        relation = relation.drop('key', axis=1)
+        all_table = pandas.DataFrame(conn.execute(sqlalchemy.sql.text("""
+            SELECT 
+                TABLE_NAME as table_name
+            FROM 
+                INFORMATION_SCHEMA.TABLES
+            WHERE
+                TABLE_SCHEMA = '{schema}'
+                AND
+                TABLE_TYPE = 'BASE TABLE';
+            """.format(schema=self.schema))))
         return (relation,all_table)
 
 # engine runner
-relation, all_table = table_and_relation(product,host,port,user,pwd,db,schema).run_engine()
+relation, all_table = table_and_relation(product,host,port,user,pwd,db,schema,envi).run_engine()
 
-# creating level 1 as base
-level_0 = pandas.merge(all_table,relation, on='table_name', how='left')
-level = level_0.loc[level_0['references'].isnull()].drop('references',axis=1)
-level['LEVEL'] = '1'
+# level 1 (table without relation)
+table_with_relation = pandas.DataFrame(pandas.concat([relation['table_name'],relation['references']],ignore_index=True).drop_duplicates(),columns=['table name'])
+level_1 = pandas.merge(all_table,table_with_relation,left_on='table_name',right_on='table name',how='left')
+level = level_1[level_1['table name'].isna()].drop('table name',axis=1)
+level['LEVEL'] = 'LV 1'
+
+# creating level 3 as base (the most outer parent)
+level_1 = pandas.merge(level_1[level_1['table name'].notna()].drop('table name',axis=1),relation, on='table_name', how='left')
+level_3 = level_1.loc[level_1['references'].isnull()].drop('references',axis=1)
+level_3['LEVEL'] = 'LV 3'
+level = pandas.concat([level,level_3])
 
 # detecting relation to self
-detector = level_0.dropna(subset='references').drop_duplicates(subset=['table_name','references'])
+detector = relation.dropna(subset='references').drop_duplicates(subset=['table_name','references'])
 
 def self_relation_identifier(arg):
     if arg['table_name'] == arg['references']:
@@ -141,14 +179,15 @@ def self_relation_identifier(arg):
 detector['self_relation'] = detector.apply(self_relation_identifier,axis=1)
 detector_pivot = detector.pivot_table(values='references',index='table_name',columns='self_relation',aggfunc='count').reset_index()
 if 'Y' in detector_pivot.columns:
-    level_1 = detector_pivot.loc[detector_pivot['X'].isnull()].drop(['X','Y'],axis=1)
-    level = pandas.concat([level,level_1])
+    level_2 = detector_pivot.loc[detector_pivot['X'].isnull()].drop(['X','Y'],axis=1)
+    level_2['LEVEL'] = 'LV 2'
+    level = pandas.concat([level,level_2])
     non_leveled = detector_pivot.loc[detector_pivot['X'].notnull()].drop(['X','Y'],axis=1)
 else:
     non_leveled = detector_pivot.loc[detector_pivot['X'].notnull()].drop(['X'],axis=1)
 
-level_counter = 2
-while non_leveled.shape[0] > 0 and level_counter < 11:
+level_counter = 4
+while non_leveled.shape[0] > 0:
     level_x = pandas.merge(non_leveled,relation,on='table_name', how='left')
     level_x['self_relation'] = level_x.apply(self_relation_identifier,axis=1)
     level_x = level_x.loc[level_x['self_relation']=='X'].drop('self_relation',axis=1) 
@@ -167,15 +206,17 @@ while non_leveled.shape[0] > 0 and level_counter < 11:
     level_x = level_x.reset_index()
     if 'X' in level_x.columns:
         new_data = level_x.loc[level_x['X'].isnull()].drop(['X','Y'],axis=1)
-        new_data['LEVEL'] = '{}'.format(level_counter)
+        new_data['LEVEL'] = 'LV {}'.format(level_counter)
         level = pandas.concat([level,new_data])
         not_defined_data = level_x[level_x['X'].notna()].drop(['X','Y'],axis=1)
         non_leveled = not_defined_data
         level_counter = level_counter + 1
     else:
         new_data = level_x.drop('Y',axis=1)
-        new_data['LEVEL'] = '{}'.format(level_counter)
+        new_data['LEVEL'] = 'LV {}'.format(level_counter)
         level = pandas.concat([level,new_data])
         non_leveled = pandas.DataFrame(columns=['table_name','LEVEL'])
 level = level.rename(columns={"table_name": "table name"})
+
+level = level.sort_values(by=['LEVEL', 'table name'],ascending=[True, True])
 level.to_csv(path_or_buf=root+'\\table level measure\output\{schema} table level {current_timestamp}.csv'.format(schema=schema,current_timestamp=current_timestamp),sep='|',index=False)
