7648ba948cd9ee165df780603a58729cdb48811c
Willy Yeremi <willyyeremi@gmail.com>
2024-04-01 11:46:07 +0700

    build: Update target_access.py

--- target_access.py
+++ target_access.py
@@ -1,9 +1,9 @@
 # target credential
-host = 'clstrdb.balitower.co.id'
-port = '3306'
-user = 'afhive.pratama'
-pwd = 'y3ki5piH3_p!'
-db = 'intranet.balitower.co.id'
+host = ''
+port = ''
+user = ''
+pwd = ''
+db = ''
 
 # path to oracle instantclient, if using oracle
 envi=''
