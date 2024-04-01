97d700a3efbefb06779887fa531af551abc3431f
Willy Yeremi <willyyeremi@gmail.com>
2024-04-01 11:45:19 +0700

    build: Update source_access.py

--- source_access.py
+++ source_access.py
@@ -1,9 +1,1 @@
-# source credential
-host = '10.13.0.37'
-port = '1521'
-user = 'CRESTELBILLINGPRD623'
-pwd = 'CRESTELBILLINGPRD623'
-db = 'ocspri'
 
-# path to oracle instantclient, if using oracle
-envi='C:\oracle\instantclient_21_12'
