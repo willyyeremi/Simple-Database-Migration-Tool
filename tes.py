f844e700f67658fe0637ebc9ede4afc6cf859378
Willy Yeremi <willyyeremi@gmail.com>
2024-06-19 14:07:07 +0700

    build:

--- tes.py
+++
@@ -1,7 +0,0 @@
-import module
-
-credential_data, credential_dict = module.credential_get() 
-module.credential_check(credential_data)
-# module.credential_object_maker(credential_dict)
-
-module.runner(credential_dict)
