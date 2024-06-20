1e9adec6dabf0d9503aae462d0c4358b71a5bc1f
Willy Yeremi <willyyeremi@gmail.com>
2024-06-20 17:10:35 +0700

    build: Refactor data type transformation functions in Python.
     - This MR refactors the data type transformation functions in Python by updating the code to enhance readability and maintainability.

--- module/data_type_mapper/mysql.py
+++ module/data_type_mapper/mysql.py
@@ -4,7 +4,7 @@ def tinyint(product_target: str, metadata_dict: dict[str: str]) -> str:
     
     Args:
         - product_target(string): the product name of destination
-        - number_type_attribute(string): tinyint attibutes argument
+        - metadata_dict(dictionary): dictionary of column metadata
     
     Returns:
         target_script(string): string for the column ddl
@@ -24,7 +24,7 @@ def smallint(product_target: str, metadata_dict: dict[str: str]) -> str:
     
     Args:
         - product_target(string): the product name of destination
-        - number_type_attribute(string): smallint attibutes argument
+        - metadata_dict(dictionary): dictionary of column metadata
     
     Returns:
         target_script(string): string for the column ddl
@@ -44,7 +44,7 @@ def mediumint(product_target: str, metadata_dict: dict[str: str]) -> str:
     
     Args:
         - product_target(string): the product name of destination
-        - number_type_attribute(string): mediumint attibutes argument
+        - metadata_dict(dictionary): dictionary of column metadata
     
     Returns:
         target_script(string): string for the column ddl
@@ -64,7 +64,7 @@ def int(product_target: str, metadata_dict: dict[str: str]) -> str:
     
     Args:
         - product_target(string): the product name of destination
-        - number_type_attribute(string): int attibutes argument
+        - metadata_dict(dictionary): dictionary of column metadata
     
     Returns:
         target_script(string): string for the column ddl
@@ -84,7 +84,7 @@ def bigint(product_target: str, metadata_dict: dict[str: str]) -> str:
     
     Args:
         - product_target(string): the product name of destination
-        - number_type_attribute(string): bigint attibutes argument
+        - metadata_dict(dictionary): dictionary of column metadata
     
     Returns:
         target_script(string): string for the column ddl
@@ -93,7 +93,7 @@ def bigint(product_target: str, metadata_dict: dict[str: str]) -> str:
     is_nullable = metadata_dict[is_nullable]
     number_type_attribute = metadata_dict[number_type_attribute]
     extra = metadata_dict[extra]
-    if is_nullable == 'NOT NULL' and match("unsigned.+", number_type_attribute) and match(".+auto_increment.+", extra):
+    if is_nullable == 'NOT NULL' and match("unsigned.*", number_type_attribute) and match(".*auto_increment.*", extra):
         if product_target == 'mysql':
             target_script = f'bigint {number_type_attribute} auto_increment'
         elif product_target == 'postgresql':
@@ -115,7 +115,7 @@ def float(product_target: str, metadata_dict: dict[str: str]) -> str:
     
     Args:
         - product_target(string): the product name of destination
-        - number_type_attribute(string): float attibutes argument
+        - metadata_dict(dictionary): dictionary of column metadata
     
     Returns:
         target_script(string): string for the column ddl
@@ -123,7 +123,7 @@ def float(product_target: str, metadata_dict: dict[str: str]) -> str:
     if product_target == 'mysql':
         target_script = f'float'
     elif product_target == 'postgresql':
-        target_script = f'float'
+        target_script = f'real'
     elif product_target == 'mariadb':
         target_script = f'float'
     return target_script
@@ -134,12 +134,11 @@ def double(product_target: str, metadata_dict: dict[str: str]) -> str:
     
     Args:
         - product_target(string): the product name of destination
-        - number_type_attribute(string): double attibutes argument
+        - metadata_dict(dictionary): dictionary of column metadata
     
     Returns:
         target_script(string): string for the column ddl
     """
-    
     if product_target == 'mysql':
         target_script = f'double'
     elif product_target == 'postgresql':
@@ -154,7 +153,7 @@ def numeric(product_target: str, metadata_dict: dict[str: str]) -> str:
     
     Args:
         - product_target(string): the product name of destination
-        - number_type_attribute(string): numeric attibutes argument
+        - metadata_dict(dictionary): dictionary of column metadata
     
     Returns:
         target_script(string): string for the column ddl
@@ -167,4 +166,238 @@ def numeric(product_target: str, metadata_dict: dict[str: str]) -> str:
         target_script = f'numeric({numeric_precision, numeric_scale})'
     elif product_target == 'mariadb':
         target_script = f'numeric({numeric_precision, numeric_scale})'
+    return target_script
+
+def char(product_target: str, metadata_dict: dict[str: str]) -> str:
+    """
+    Transform char data type from mysql
+    
+    Args:
+        - product_target(string): the product name of destination
+        - metadata_dict(dictionary): dictionary of column metadata
+    
+    Returns:
+        target_script(string): string for the column ddl
+    """
+    char_max_length = metadata_dict[char_max_length]
+    char_set = metadata_dict[char_set]
+    char_collation = metadata_dict[char_collation]
+    if product_target == 'mysql':
+        target_script = f'char({char_max_length}) set {char_set} collation {char_collation}'
+    elif product_target == 'postgresql':
+        target_script = f'char({char_max_length})'
+    elif product_target == 'mariadb':
+        target_script = f'char({char_max_length}) set {char_set} collation {char_collation}'
+    return target_script
+
+def varchar(product_target: str, metadata_dict: dict[str: str]) -> str:
+    """
+    Transform varchar data type from mysql
+    
+    Args:
+        - product_target(string): the product name of destination
+        - metadata_dict(dictionary): dictionary of column metadata
+    
+    Returns:
+        target_script(string): string for the column ddl
+    """
+    char_max_length = metadata_dict[char_max_length]
+    char_set = metadata_dict[char_set]
+    char_collation = metadata_dict[char_collation]
+    if product_target == 'mysql':
+        target_script = f'varchar({char_max_length}) set {char_set} collation {char_collation}'
+    elif product_target == 'postgresql':
+        target_script = f'varchar({char_max_length})'
+    elif product_target == 'mariadb':
+        target_script = f'varchar({char_max_length}) set {char_set} collation {char_collation}'
+    return target_script
+
+def tinytext(product_target: str, metadata_dict: dict[str: str]) -> str:
+    """
+    Transform tinytext data type from mysql
+    
+    Args:
+        - product_target(string): the product name of destination
+        - metadata_dict(dictionary): dictionary of column metadata
+    
+    Returns:
+        target_script(string): string for the column ddl
+    """
+    char_max_length = metadata_dict[char_max_length]
+    char_set = metadata_dict[char_set]
+    char_collation = metadata_dict[char_collation]
+    if product_target == 'mysql':
+        target_script = f'tinytext({char_max_length}) set {char_set} collation {char_collation}'
+    elif product_target == 'postgresql':
+        target_script = f'text'
+    elif product_target == 'mariadb':
+        target_script = f'tinytext({char_max_length}) set {char_set} collation {char_collation}'
+    return target_script
+
+def text(product_target: str, metadata_dict: dict[str: str]) -> str:
+    """
+    Transform text data type from mysql
+    
+    Args:
+        - product_target(string): the product name of destination
+        - metadata_dict(dictionary): dictionary of column metadata
+    
+    Returns:
+        target_script(string): string for the column ddl
+    """
+    char_max_length = metadata_dict[char_max_length]
+    char_set = metadata_dict[char_set]
+    char_collation = metadata_dict[char_collation]
+    if product_target == 'mysql':
+        target_script = f'text({char_max_length}) set {char_set} collation {char_collation}'
+    elif product_target == 'postgresql':
+        target_script = f'text'
+    elif product_target == 'mariadb':
+        target_script = f'text({char_max_length}) set {char_set} collation {char_collation}'
+    return target_script
+
+def mediumtext(product_target: str, metadata_dict: dict[str: str]) -> str:
+    """
+    Transform mediumtext data type from mysql
+    
+    Args:
+        - product_target(string): the product name of destination
+        - metadata_dict(dictionary): dictionary of column metadata
+    
+    Returns:
+        target_script(string): string for the column ddl
+    """
+    char_max_length = metadata_dict[char_max_length]
+    char_set = metadata_dict[char_set]
+    char_collation = metadata_dict[char_collation]
+    if product_target == 'mysql':
+        target_script = f'mediumtext({char_max_length}) set {char_set} collation {char_collation}'
+    elif product_target == 'postgresql':
+        target_script = f'text'
+    elif product_target == 'mariadb':
+        target_script = f'mediumtext({char_max_length}) set {char_set} collation {char_collation}'
+    return target_script
+
+def longtext(product_target: str, metadata_dict: dict[str: str]) -> str:
+    """
+    Transform longtext data type from mysql
+    
+    Args:
+        - product_target(string): the product name of destination
+        - metadata_dict(dictionary): dictionary of column metadata
+    
+    Returns:
+        target_script(string): string for the column ddl
+    """
+    char_max_length = metadata_dict[char_max_length]
+    char_set = metadata_dict[char_set]
+    char_collation = metadata_dict[char_collation]
+    if product_target == 'mysql':
+        target_script = f'longtext({char_max_length}) set {char_set} collation {char_collation}'
+    elif product_target == 'postgresql':
+        target_script = f'text'
+    elif product_target == 'mariadb':
+        target_script = f'longtext({char_max_length}) set {char_set} collation {char_collation}'
+    return target_script
+
+def binary(product_target: str, metadata_dict: dict[str: str]) -> str:
+    """
+    Transform binary data type from mysql
+    
+    Args:
+        - product_target(string): the product name of destination
+        - metadata_dict(dictionary): dictionary of column metadata
+    
+    Returns:
+        target_script(string): string for the column ddl
+    """
+    char_max_size = metadata_dict[char_max_size]
+    char_set = metadata_dict[char_set]
+    char_collation = metadata_dict[char_collation]
+    if product_target == 'mysql':
+        target_script = f'binary({char_max_size})'
+    elif product_target == 'postgresql':
+        target_script = f'bytea'
+    elif product_target == 'mariadb':
+        target_script = f'binary({char_max_size})'
+    return target_script
+
+def tinyblob(product_target: str, metadata_dict: dict[str: str]) -> str:
+    """
+    Transform tinyblob data type from mysql
+    
+    Args:
+        - product_target(string): the product name of destination
+        - metadata_dict(dictionary): dictionary of column metadata
+    
+    Returns:
+        target_script(string): string for the column ddl
+    """
+    char_max_size = metadata_dict[char_max_size]
+    if product_target == 'mysql':
+        target_script = f'tinyblob'
+    elif product_target == 'postgresql':
+        target_script = f'bytea'
+    elif product_target == 'mariadb':
+        target_script = f'tinyblob'
+    return target_script
+
+def blob(product_target: str, metadata_dict: dict[str: str]) -> str:
+    """
+    Transform blob data type from mysql
+    
+    Args:
+        - product_target(string): the product name of destination
+        - metadata_dict(dictionary): dictionary of column metadata
+    
+    Returns:
+        target_script(string): string for the column ddl
+    """
+    char_max_size = metadata_dict[char_max_size]
+    if product_target == 'mysql':
+        target_script = f'blob({char_max_size})'
+    elif product_target == 'postgresql':
+        target_script = f'bytea'
+    elif product_target == 'mariadb':
+        target_script = f'blob({char_max_size})'
+    return target_script
+
+def mediumblob(product_target: str, metadata_dict: dict[str: str]) -> str:
+    """
+    Transform mediumblob data type from mysql
+    
+    Args:
+        - product_target(string): the product name of destination
+        - metadata_dict(dictionary): dictionary of column metadata
+    
+    Returns:
+        target_script(string): string for the column ddl
+    """
+    char_max_size = metadata_dict[char_max_size]
+    if product_target == 'mysql':
+        target_script = f'mediumblob'
+    elif product_target == 'postgresql':
+        target_script = f'bytea'
+    elif product_target == 'mariadb':
+        target_script = f'mediumblob'
+    return target_script
+
+def longblob(product_target: str, metadata_dict: dict[str: str]) -> str:
+    """
+    Transform longblob data type from mysql
+    
+    Args:
+        - product_target(string): the product name of destination
+        - metadata_dict(dictionary): dictionary of column metadata
+    
+    Returns:
+        target_script(string): string for the column ddl
+    """
+    char_max_size = metadata_dict[char_max_size]
+    if product_target == 'mysql':
+        target_script = f'longblob'
+    elif product_target == 'postgresql':
+        target_script = f'bytea'
+    elif product_target == 'mariadb':
+        target_script = f'longblob'
     return target_script
